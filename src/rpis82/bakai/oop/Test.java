2e44bc382303b68774bd769c012567a8814dae3a
JuliaBak <ov.bakai@gmail.com>
2020-02-24 15:01:59 +0400

    Added entry point

--- src/rpis82/bakai/oop/Test.java
+++ src/rpis82/bakai/oop/Test.java
@@ -2,6 +2,6 @@ package rpis82.bakai.oop;
 
 public class Test {
     public static void main (String[] args) {
-        System.out.println("Я сделяль!");
+        System.out.println("Я сделял!");
     }
 }
