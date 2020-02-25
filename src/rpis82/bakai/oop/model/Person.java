3e20a52467cfd7a06f0accd027b8a99fa9760ddc
JuliaBak <ov.bakai@gmail.com>
2020-02-25 23:28:33 +0400

    added some persons

--- src/rpis82/bakai/oop/model/Person.java
+++ src/rpis82/bakai/oop/model/Person.java
@@ -2,7 +2,7 @@ package rpis82.bakai.oop.model;
 
 public class Person {
     String Surname, Name;
-
+    public final Person person = new Person("", "");
     public Person(String Surname, String Name)
     {
         this.Name = Name;
@@ -23,7 +23,10 @@ public class Person {
         return Surname;
     }
     public static void main(String[] args) {
+
         Person person1 = new Person("Bakai", "Julia");
+        Person person2 = new Person("Ivanov", "Sergey");
+        Person person3 = new Person("Kirov", "Vasia");
         System.out.println(person1.Name);
         System.out.println(person1.Surname);
     }
