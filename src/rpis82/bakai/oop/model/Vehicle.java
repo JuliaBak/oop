f223604bf1595ed9b7f002621317c26b0b27c047
JuliaBak <ov.bakai@gmail.com>
2020-02-25 23:35:17 +0400

    added methods

--- src/rpis82/bakai/oop/model/Vehicle.java
+++ src/rpis82/bakai/oop/model/Vehicle.java
@@ -1,4 +1,41 @@
 package rpis82.bakai.oop.model;
 
 public class Vehicle {
+    int gosNumber;
+    String producer, model;
+
+    public Vehicle()
+    {
+
+    }
+    public Vehicle(int gosNumber, String producer, String model)
+    {
+        this.gosNumber = gosNumber;
+        this.producer = producer;
+        this.model = model;
+    }
+
+    public int getGosNumber() {
+        return gosNumber;
+    }
+
+    public void setGosNumber(int gosNumber) {
+        this.gosNumber = gosNumber;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
