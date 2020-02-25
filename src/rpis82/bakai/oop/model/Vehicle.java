47a9eba603e94ebceef9daf3bc5497c8802d139c
JuliaBak <ov.bakai@gmail.com>
2020-02-25 23:41:28 +0400

    added vehicles

--- src/rpis82/bakai/oop/model/Vehicle.java
+++ src/rpis82/bakai/oop/model/Vehicle.java
@@ -38,4 +38,14 @@ public class Vehicle {
     public void setModel(String model) {
         this.model = model;
     }
+    public static void main(String[] args) {
+
+        Vehicle vehicle1 = new Vehicle(432, "Toyota", "Corolla");
+        Vehicle vehicle2 = new Vehicle(574, "BMW", "X6");
+        Vehicle vehicle3 = new Vehicle(762, "Ferrari", "California");
+
+        System.out.println(vehicle1.gosNumber);
+        System.out.println(vehicle1.model);
+        System.out.println(vehicle1.producer);
+    }
 }
